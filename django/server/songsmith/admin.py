b750fa61997794b1aaa22040198391213fd9dbec
mehrin <mehrinshamim4869@gmail.com>
2025-03-27 17:49:02 +0530

    delete django dir

--- django/server/songsmith/admin.py
+++
@@ -1,9 +0,0 @@
-#songsmith/admin.py
-from django.contrib import admin
-from .models import Profile, ListeningContext, SongDetail, PlayHistory
-
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(ListeningContext)
-admin.site.register(SongDetail)
-admin.site.register(PlayHistory)
---
+++ server/debugging.md
@@ -0,0 +1,7 @@
+reasons for fastapi not loading so far
+
+- frontend url port mistaken
+- if __name__ == "__main__" 
+- async fns where they are not needed
+- not adding init files
+
