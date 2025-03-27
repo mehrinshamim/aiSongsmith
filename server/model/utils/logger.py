b84c7cd31e6241aba332a31ba3a409aca5fd666d
mehrin <mehrinshamim4869@gmail.com>
2025-03-27 17:27:51 +0530

    refactor(server): remove unused files and configurations for cleaner structure

--- server/model/utils/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-from pathlib import Path
-
-def setup_logger(name: str, log_dir: Path):
-    """Set up logger for training/inference"""
-    logger = logging.getLogger(name)
-    # Configure logger
-    return logger
